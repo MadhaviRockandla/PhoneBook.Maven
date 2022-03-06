25cf74b0e1956945e4d57839d01eadaa5331becd
MadhaviRockandla <rokandla@gmail.com>
2022-03-06 17:59:46 -0500

    Lab Done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
  */
 public class PhoneBook {
 
-    private static  Map<String, List<String>> phonebook;
+    private static Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
         phonebook = map;
@@ -22,12 +22,12 @@ public class PhoneBook {
 
     public static void add(String name, String phoneNumber) {
         List<String> phoneNumList = Arrays.asList(phoneNumber);
-        phonebook.put(name,phoneNumList);
+        phonebook.put(name, phoneNumList);
     }
 
     public void addAll(String name, String... phoneNumbers) {
         List<String> phoneNumList = Arrays.asList(phoneNumbers);
-        phonebook.put(name,phoneNumList);
+        phonebook.put(name, phoneNumList);
     }
 
     public void remove(String name) {
@@ -42,9 +42,9 @@ public class PhoneBook {
         return phonebook.get(name);
     }
 
-    public String reverseLookup(String phoneNumber)  {
-        for (Map.Entry<String, List<String>> entry : phonebook.entrySet()){
-            if(entry.getValue().contains(phoneNumber)){
+    public String reverseLookup(String phoneNumber) {
+        for (Map.Entry<String, List<String>> entry : phonebook.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
                 return entry.getKey();
             }
         }
@@ -53,12 +53,15 @@ public class PhoneBook {
 
     public List<String> getAllContactNames() {
         List<String> returnList = new ArrayList<>();
-        for(String i : phonebook.keySet()){
+        for (String i : phonebook.keySet()) {
             returnList.add(i);
         }
         return returnList;
     }
 
+    public Map<String, List<String>> getMap() {
+        return phonebook;
 
     }
 
+}
